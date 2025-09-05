c1fde357eed535a85b5440d1fdb1a6001999b5c2
stfwr-dvlpr <mohammadaquib890@gmail.com>
2025-09-05 12:29:34 +0530

    add cors to connect with frontend

--- index.js
+++ index.js
@@ -4,10 +4,27 @@ const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
 const connectDB = require('./config/db');
 const passport = require('passport');
+const cors = require('cors');
 require('./config/auth');
 
 dotenv.config();
 const app = express();
+
+
+const allowedOrigins = ['https://home-tutor-finder-ov4x13dwi-md-aquibs-projects.vercel.app/'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+};
+
+app.use(cors(corsOptions));
 app.use(session({secret: 'cats'}));
 app.use(passport.initialize());
 app.use(passport.session());
